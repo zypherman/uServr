7345c1df89c7d905567850f43d7fa667b0f29334
John Anderson <jra.hockey4@gmail.com>
2015-10-02 19:27:45 -0500

    Fixing the redirect to the customer index. Changed it to forward the request instead of redirect the redirect request which grails was blowing up about.

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -3,7 +3,6 @@ import com.uservr.web.*
 class BootStrap {
 
     def init = { servletContext ->
-        //new Customer(username: "bob@bsb.com",password:"password", name:"Bob Buttlicker").save(failOnError: true)
         Customer.findOrSaveWhere(username: "bill",password:"password", name:"Bill Buttlicker")
     }
     def destroy = {
