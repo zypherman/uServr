d987ca8d1e973b0a6a40d7ddfaa1457993e2cff1
John Anderson <jra.hockey4@gmail.com>
2015-10-04 01:21:50 -0500

    Adding postgresql dependencies for heroku deploy

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,8 +1,20 @@
-import com.uservr.web.*
+import com.uservr.web.Customer
+import grails.plugin.heroku.PostgresqlServiceInfo
 
 class BootStrap {
 
     def init = { servletContext ->
+        String DATABASE_URL = System.getenv('DATABASE_URL')
+        if (DATABASE_URL) {
+            try {
+                PostgresqlServiceInfo info = new PostgresqlServiceInfo()
+                println "nPostgreSQL service ($DATABASE_URL): url='$info.url', " +
+                        "user='$info.username', password='$info.password'n"
+            }
+            catch (e) {
+                println "Error occurred parsing DATABASE_URL: $e.message"
+            }
+        }
         Customer.findOrSaveWhere(username: "bill",password:"password", name:"Bill Buttlicker")
     }
     def destroy = {
