976f1ff1303b42047849b7bbd791ccdff34469fc
Anderson, John <John.Anderson@bluestembrands.com>
2015-10-07 13:50:27 -0500

    Turning off session filters for demonstration purposes

--- grails-app/conf/filters/SecurityFilters.groovy
+++ grails-app/conf/filters/SecurityFilters.groovy
@@ -5,6 +5,10 @@ class SecurityFilters {
     def filters = { //Set to check for all log in
         loginCheck(controller: '*', action: '*') {
             before = {
+                //turn off authentication
+                session.customer = "bob"
+                session.name = "Bob Buttlicker"
+
                 if (!session.customer && (!controllerName.equals('login'))) {
                     redirect(controller: 'login', action: 'login')
                     return false
