d0e30977bb48ae0f0061ac17d4494e8934790698
Anderson, John <John.Anderson@bluestembrands.com>
2015-10-28 13:22:50 -0500

    Reverting to postgresql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,23 +29,23 @@ environments {
         }
     }
 
-    production {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
-        }
-    }
 //    production {
 //        dataSource {
-//            dbCreate = "update"
-//            driverClassName = "org.postgresql.Driver"
-//            dialect = org.hibernate.dialect.PostgreSQLDialect
-//
-//            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-//
-//            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
-//            username = uri.userInfo.split(":")[0]
-//            password = uri.userInfo.split(":")[1]
+//            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+//            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
 //        }
 //    }
+    production {
+        dataSource {
+            dbCreate = "update"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+
+            uri = new URI(System.env.DATABASE_URL ?: "postgres://test:test@localhost/test")
+
+            url = "jdbc:postgresql://" + uri.host + ":" + uri.port + uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
+        }
+    }
 }
