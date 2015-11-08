0acfcd2dae6ca0e091cdd19fd4d9991e42adfd47
John Anderson <John@Johns-MacBook-Pro-2.local>
2015-11-08 12:48:15 -0600

    Rewrite of login controller and adding of login service. Created a user class to handle our workers who are not customers. Reworked customer class to represent a customer better. Created customer register modal. Added AddCreditCard modal that can pop up. Changed send order behavior to have user register first.

--- test/unit/com/uservr/web/UserSpec.groovy
+++ test/unit/com/uservr/web/UserSpec.groovy
@@ -6,8 +6,8 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(Payment)
-class PaymentSpec extends Specification {
+@TestFor(User)
+class UserSpec extends Specification {
 
     def setup() {
     }
